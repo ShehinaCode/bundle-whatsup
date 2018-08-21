f78c27cc9a0db14102aae9d949532b99f1abb689
Andrew <codcore@gmail.com>
2018-08-21 04:48:40 +0300

    Add stuff neede for bundler plugin system due to 'https://bundler.io/v1.16/guides/bundler_plugins.html'

--- lib/bundle/whatsup.rb
+++ lib/bundle/whatsup.rb
@@ -1,7 +1,15 @@
-require "bundle/whatsup/version"
-
+require_relative './whatsup/version.rb'
+require 'bundler'
 module Bundle
   module Whatsup
-    # Your code goes here...
+    class Whatsup < Bundler::Plugin::API
+      # Register bundle whatsup command
+      command "my_command"
+
+      # this method is called when we run 'bundle whatsup'
+      def exec(command, args)
+        puts "Hello, I'm 'command' command"
+      end
+    end
   end
 end
