fb9fbcfc53c3e83bbbc39014cae31da38452c423
Andrew <codcore@gmail.com>
2018-08-23 00:05:20 +0300

    Delete bundle folder

---
+++ lib/bundler/whatsup/gem_parser.rb
@@ -0,0 +1,19 @@
+require 'bundler'
+
+module Bundle
+  module Whatsup
+    class GemfileParser
+
+      # Returns Hash with gems name and version
+      def self.get_gems_versions
+        specs = Bundler.load.specs.sort_by(&:name)
+        gems = {}
+        specs.map do |gem|
+            gems[gem.name.to_sym] = gem.version.to_s
+        end
+
+        p gems
+      end
+    end
+  end
+end
