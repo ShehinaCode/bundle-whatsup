876f069d2a89a4ff7e95b2eda8cfdc90f9b9464d
Andrew <codcore@gmail.com>
2018-09-18 22:27:52 +0300

    Add:spec: for Gemfile#get_version

--- spec/bundler/whatsup/gemfile_spec.rb
+++ spec/bundler/whatsup/gemfile_spec.rb
@@ -6,6 +6,11 @@ describe Bundler::Whatsup::Gemfile do
   let(:gemfile) { Bundler::Whatsup::Gemfile.new }
   let(:version_regexp) { /(\d{1,3}.\d{1,3}(.\d{1,3})?)/ }
 
+  describe '#get_version' do
+    it { expect(gemfile.get_version('some_gem')).to be_nil }
+    it { expect(gemfile.get_version('bundler-whatsup')).to be_a String }
+  end
+
   describe '#gems_versions' do
     it 'returns a Hash' do
       expect(gemfile.specs_versions).to be_a Hash
